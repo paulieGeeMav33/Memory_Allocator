04d3cb59f21c0343c04d67aefde15612c5797fa2
Paul Goins III <122927048+paulieGeeMav33@users.noreply.github.com>
2023-10-24 22:31:29 +0000

    Creating benchmarks.

--- benchmark1.c
+++ benchmark1.c
@@ -1,7 +1,26 @@
 #include "memalloc.h"
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main( int argc, char * argv[] )
 {
 
+
+  struct timeval begin;
+  struct timeval end;
+
+  gettimeofday(&begin,NULL);
+  char* ptr = malloc(1000);
+  size_t count = 2000;
+  for (size_t i = 0; i < count; i++)
+  {
+    free(ptr);
+    ptr = malloc(1000);
+  }
+  gettimeofday(&end,NULL);
+  double duration = ((end.tv_sec * 1000000) + end.tv_usec) - ((begin.tv_sec * 1000000) + begin.tv_usec);
+  printf("duration is:%f\n",duration);
+  
   return 0;
 }
--- benchmark2.c
+++ benchmark2.c
@@ -1,7 +1,31 @@
 #include "memalloc.h"
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 
 int main( int argc, char * argv[] )
 {
+  struct timeval begin;
+  struct timeval end;
 
+  gettimeofday(&begin,NULL);
+  int success = memalloc_init(1000,FIRST_FIT);
+  if (success == -1)
+  {
+    return success;
+  }
+  
+  char* ptr = memalloc_alloc(1000);
+  size_t count = 2000;
+  for (size_t i = 0; i < count; i++)
+  {
+    free(ptr);
+    ptr = memalloc_alloc(1000);
+  }
+  gettimeofday(&end,NULL);
+  double duration = ((end.tv_sec * 1000000) + end.tv_usec) - ((begin.tv_sec * 1000000) + begin.tv_usec);
+  printf("duration is:%f\n",duration);
+  
   return 0;
 }
