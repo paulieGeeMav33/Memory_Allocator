6c98f674cc71024df3d2d6498892bac238c860d9
Paul Goins III <122927048+paulieGeeMav33@users.noreply.github.com>
2023-10-25 06:42:27 +0000

    Added comments explaining what each test will do.

--- benchmark1.c
+++ benchmark1.c
@@ -13,6 +13,16 @@ int main( int argc, char * argv[] )
 
   gettimeofday(&begin,NULL);
   
+  //Test 1
+
+  //Allocating and freeing a lot of pointers
+
+  /*int status = memalloc_init(count,FIRST_FIT);
+  if (status == -1)
+  {
+    return status;
+  }*/
+
   size_t count = 10000;
   char* ptr[count];
   for (size_t i = 0; i < count; i++)
@@ -28,6 +38,10 @@ int main( int argc, char * argv[] )
   gettimeofday(&end,NULL);
   double duration = ((end.tv_sec * 1000000) + end.tv_usec) - ((begin.tv_sec * 1000000) + begin.tv_usec);
   printf("duration is:%f\n",duration);
+
+  //Test two allocating half of the heap and freeing all even pointers
+
+  //Test three allocating all of the heap freeing half than adding it back in
   
   return 0;
 }
